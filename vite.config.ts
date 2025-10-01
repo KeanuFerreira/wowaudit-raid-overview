0868dbf84f74962d7b983820f26bc6d169909731
Keanu Ferreira <k.ferreira@bueltel.com>
2025-10-01 12:39:28 +0200

    Remove unused environment variable definitions from Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig(() => {
     return {
-        define: {
-            __APP_ENV__: import.meta.env.VITE_VERCEL_ENV,
-            __WOWAUDIT_CREDENTIAL__: import.meta.env.WOWAUDIT_CREDENTIAL,
-            __WOWAUDIT_API_URL__: import.meta.env.WOWAUDIT_API_URL
-        },
         plugins: [react()],
     };
 });
