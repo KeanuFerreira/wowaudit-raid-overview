03cebc18289fbf834cf15a1e69146cd6c263a0e1
Keanu Ferreira <k.ferreira@bueltel.com>
2025-10-01 13:42:17 +0200

    Add raid data fetching and character loading in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,44 @@
 import './App.css'
 import RaidSelect from "./raid-select/RaidSelect.tsx";
+import {useEffect, useState} from "react";
+
+
+const fetchRaidData = async (raidId) => {
+    return fetch(`/api/wowaudit?targetRoute=${encodeURIComponent(`raids/${raidId}`)}`)
+        .then(response => response.json())
+}
 
 function App() {
     // Access Vercel environment variables exposed via VITE_ prefix
+    const [selectedRaid, setSelectedRaid] = useState(null);
+    const [selectedRaidData, setSelectedRaidData] = useState(null);
+    const [characters, setCharacters] = useState([]);
 
+    useEffect(() => {
+        // load all characters from /api/wowaudit?targetRoute=${encodeURIComponent('characters')}
+        fetch(`/api/wowaudit?targetRoute=${encodeURIComponent('characters')}`)
+            .then(response => response.json())
+            .then(data => {
+                setCharacters(data);
+            });
+    }, []);
+
+
+    useEffect(() => {
+        // load the selected raid from /api/wowaudit?targetRoute=${encodeURIComponent(`raids/${raidId}`)}
+        // @ts-ignore
+        fetchRaidData(selectedRaid.id).then(data => {
+            console.log(data)
+            setSelectedRaidData(data);
+        })
+    }, [selectedRaid]);
+    console.log(selectedRaidData, characters)
     return (
         <>
-            <RaidSelect/>
+            <RaidSelect
+                // @ts-ignore
+                onChange={setSelectedRaid}
+            />
         </>
     )
 }
