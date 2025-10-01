c7226f0aa8642dd4da3db0df6ba211c862462210
Keanu Ferreira <k.ferreira@bueltel.com>
2025-10-01 12:22:49 +0200

    Add environment variable definitions for WOWAUDIT in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig(() => {
-  return {
-    plugins: [react()],
-  };
+    return {
+        define: {
+            __APP_ENV__: import.meta.env.VITE_VERCEL_ENV,
+            __WOWAUDIT_CREDENTIAL__: import.meta.env.WOWAUDIT_CREDENTIAL,
+            __WOWAUDIT_API_URL__: import.meta.env.WOWAUDIT_API_URL
+        },
+        plugins: [react()],
+    };
 });
