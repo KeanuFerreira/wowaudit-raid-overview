01725bd50b2314769446029f15656490ae30bf65
Keanu Ferreira <k.ferreira@bueltel.com>
2025-10-01 13:12:24 +0200

    Update GET function to fetch and return raid data from external API

--- api/hello.ts
+++ api/hello.ts
@@ -1,4 +1,18 @@
-export function GET(request: Request) {
+export async function GET(request: Request) {
     console.log(request)
-    return new Response('Hello from Vercel!');
+
+    //request wowaudit raids to check if it works
+    const myHeaders = new Headers();
+    myHeaders.append("accept", "application/json");
+    myHeaders.append("Authorization", "a0a3875d2392fb8fc8a00a6ce116f2cbadd36a4f0199e6840bf7b16b670570f5");
+
+    const requestOptions = {
+        method: "GET",
+        headers: myHeaders,
+    };
+
+    const response = await fetch("https://wowaudit.com/v1/raids?include_past=false", requestOptions);
+    const result = await response?.json();
+    console.log(result)
+    return new Response(result);
 }
