e341f613ca437aa1b5d413ed4dbd91006a9a5680
Keanu Ferreira <k.ferreira@bueltel.com>
2025-10-01 13:33:44 +0200

    Refactor App component to remove unnecessary heading and simplify structure

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,13 @@ import './App.css'
 import RaidSelect from "./raid-select/RaidSelect.tsx";
 
 function App() {
-  // Access Vercel environment variables exposed via VITE_ prefix
+    // Access Vercel environment variables exposed via VITE_ prefix
 
-  return (
-    <>
-      <h1>Vite + React</h1>
-        <RaidSelect/>
-    </>
-  )
+    return (
+        <>
+            <RaidSelect/>
+        </>
+    )
 }
 
 export default App
